26bd00179e7a4501db526b010de093d8bac56bd6
deadchri5 <deadchri5h@gmail.com>
2019-10-02 14:02:04 -0500

    Correcion en PHP de Registro

--- PHP/AñadirUsuario/anadirUsuario.php
+++ PHP/AñadirUsuario/anadirUsuario.php
@@ -3,21 +3,31 @@
 $conexion=mysqli_connect("localhost","root","")
 or die("No se puede conectar con el servidor");
 
-mysqli_select_db($conexion, "skate")
+mysqli_select_db($conexion, "skaters")
 or die("No se puede conectar a la base de datos.");
 
 
 $nombre=$_POST["nombre"];
 $apellido=$_POST["apellido"];
 $correo=$_POST["correo"];
-$contraseña=$_POST["contrasena"];
+$contraseña=$_POST["contraseña"];
+$confirmar=$_POST["confirmar"];
 
+if ($contraseña != $confirmar){
+    echo 'Verifica la contraseña';
+    header('Location: ../../../../sketis/Skate/Vistas/skaters/registro.html');
+}
 
-$consulta = mysqli_query($conexion,'insert into Usuario (Nombre, Apellido, Correo, Contraseña) 
-values ("'.$nombre.'", "'.$apellido.'", "'.$correo.'",  aes_encrypt("'.$contraseña.'", "'.$_SESSION['secret'].'"));')
-or die("Fallo la consulta");
+else{
+    $cuentaFilas = mysqli_query($conexion, "select Usuario_ID from usuario") or die ("Fallo la consulta uno");
+    $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
 
-echo 'Success!';
+    $consulta = mysqli_query($conexion, "insert into usuario (Usuario_ID, Tipo_FK, Nombre, Apellido, Correo, Contrasena)
+    values ('$nUsuario', '1', '$nombre', '$apellido', '$correo', '$contraseña')")
+    or die("Fallo la consulta");
+
+    echo 'Registro Finalizado!';
+}
 
 mysqli_close($conexion);
 ?>
