9363e44ff23c59cb53407446af8b6675cf455e53
deadchri5 <deadchri5h@gmail.com>
2019-10-02 21:43:05 -0500

    Modifique el php de registro, ahora no acepta correos iguales.

--- PHP/AñadirUsuario/anadirUsuario.php
+++ PHP/AñadirUsuario/anadirUsuario.php
@@ -1,11 +1,13 @@
 <?PHP
 
+/**
+ * @ author deadchri5
+ */
 $conexion=mysqli_connect("localhost","root","")
 or die("No se puede conectar con el servidor");
 
 mysqli_select_db($conexion, "skaters")
 or die("No se puede conectar a la base de datos.");
-
 
 $nombre=$_POST["nombre"];
 $apellido=$_POST["apellido"];
@@ -15,19 +17,28 @@ $confirmar=$_POST["confirmar"];
 
 if ($contraseña != $confirmar){
     echo 'Verifica la contraseña';
-    header('Location: ../../../../sketis/Skate/Vistas/skaters/registro.html');
+    header('Location: ../../../../Skate/Skate/Vistas/skaters/registro.html');
 }
-
 else{
     $cuentaFilas = mysqli_query($conexion, "select Usuario_ID from usuario") or die ("Fallo la consulta uno");
     $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
 
+    $verificaCorreo = mysqli_query($conexion, "select Correo from usuario") or die ("Fallo en la consulta correo");
+    $verifica=mysqli_fetch_array($verificaCorreo);
+
     $consulta = mysqli_query($conexion, "insert into usuario (Usuario_ID, Tipo_FK, Nombre, Apellido, Correo, Contrasena)
     values ('$nUsuario', '1', '$nombre', '$apellido', '$correo', '$contraseña')")
-    or die("Fallo la consulta");
+    or die("Fallo la consulta </br>");
 
-    echo 'Registro Finalizado!';
+    $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
+    for ($i = 1; $i <= $nUsuario; $i ++){
+        if ($correo == $verifica["Correo"]){
+            echo 'Error (666). El correo ya esta registrado en el sistema.';
+            $borrarIncorrectos = mysqli_query($conexion, "delete from usuario where Usuario_ID = '$nUsuario'") or die ("fallo al borrar rep");
+        }
+        $verifica=mysqli_fetch_array($verificaCorreo);
+    }
+    
 }
-
 mysqli_close($conexion);
 ?>
