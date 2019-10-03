122326b2a6cd6ce950251665daed8ac25e3b93b7
deadchri5 <deadchri5h@gmail.com>
2019-10-02 23:39:54 -0500

    PHP de vista Registro almacenista (Adriana) <3

---
+++ PHP/regAlmacenistas.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * author Liloth00814
+ */
+
+$conexion=mysqli_connect("localhost","root","")
+or die("No se puede conectar con el servidor");
+
+mysqli_select_db($conexion, "skaters")
+or die("No se puede conectar a la base de datos.");
+
+$nombre=$_POST["nombre"];
+$apellido=$_POST["apellido"];
+$correo=$_POST["correo"];
+$contraseña=$_POST["contraseña"];
+$confirmar=$_POST["confirmar"];
+
+if ($contraseña != $confirmar){
+    echo '
+    <html>
+        <head>
+        <title>Skaters - Redireccion</title>
+        <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
+        <p style="color:#FFFF";> No ingresaste la misma contraseña en ambos campos, intentalo de nuevo....</p>
+            <script>
+            function r() { location.href="../Vistas/skaters/registro-almacenista.html"} 
+            setTimeout ("r()", 2700);
+            </script>
+        </head>
+        <body style="background-color:#494949;">
+        </body>
+    </html>
+    ';
+}
+else{
+    $cuentaFilas = mysqli_query($conexion, "select Usuario_ID from usuario") or die ("Fallo la consulta uno");
+    $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
+
+    $verificaCorreo = mysqli_query($conexion, "select Correo from usuario") or die ("Fallo en la consulta correo");
+    $verifica=mysqli_fetch_array($verificaCorreo);
+
+    $consulta = mysqli_query($conexion, "insert into usuario (Usuario_ID, Tipo_FK, Nombre, Apellido, Correo, Contrasena)
+    values ('$nUsuario', '3', '$nombre', '$apellido', '$correo', '$contraseña')")
+    or die("Fallo la consulta </br>");
+
+    $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
+    for ($i = 1; $i <= $nUsuario; $i ++){
+        if ($correo == $verifica["Correo"]){
+            echo 'Error (666). El correo ya esta registrado en el sistema.';
+            $borrarIncorrectos = mysqli_query($conexion, "delete from usuario where Usuario_ID = '$nUsuario'") or die ("fallo al borrar rep");
+        }
+        $verifica=mysqli_fetch_array($verificaCorreo);
+    } 
+}
+mysqli_close($conexion);
+?>
