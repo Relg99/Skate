e4b437dcd5533afb177dc3f4c8a35d0ee2204093
deadchri5 <deadchri5h@gmail.com>
2019-10-03 19:25:46 -0500

    Vista Registro y Registro Almacenista funcionando (Adriana y Chris).

--- PHP/sketis/anadirUsuario.php
+++ PHP/sketis/anadirUsuario.php
@@ -1,8 +1,8 @@
-<?php
-/**
- * author Liloth00814
- */
+<?PHP
 
+/**
+ * @ author deadchri5
+ */
 $conexion=mysqli_connect("localhost","root","")
 or die("No se puede conectar con el servidor");
 
@@ -23,7 +23,7 @@ if ($contraseña != $confirmar){
         <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
         <p style="color:#FFFF";> No ingresaste la misma contraseña en ambos campos, intentalo de nuevo....</p>
             <script>
-            function r() { location.href="../Vistas/skaters/registro-almacenista.html"} 
+            function r() { location.href="../../Vistas/skaters/registro.html"} 
             setTimeout ("r()", 2700);
             </script>
         </head>
@@ -40,31 +40,31 @@ else{
     $verifica=mysqli_fetch_array($verificaCorreo);
 
     $consulta = mysqli_query($conexion, "insert into usuario (Usuario_ID, Tipo_FK, Nombre, Apellido, Correo, Contrasena)
-    values ('$nUsuario', '3', '$nombre', '$apellido', '$correo', '$contraseña')")
+    values ('$nUsuario', '1', '$nombre', '$apellido', '$correo', '$contraseña')")
     or die("Fallo la consulta </br>");
 
     $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
     for ($i = 1; $i <= $nUsuario; $i ++){
         if ($correo == $verifica["Correo"]){
             echo '
-    <html>
-        <head>
-        <title>Skaters - Redireccion</title>
-        <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
-        <p style="color:#FFFF";> El correo ya esta registrado en el sistema....</p>
-            <script>
-            function r() { location.href="../Vistas/skaters/registro-almacenista.html"} 
-            setTimeout ("r()", 2700);
-            </script>
-        </head>
-        <body style="background-color:#494949;">
-        </body>
-    </html>
+            <html>
+                <head>
+                <title>Skaters - Redireccion</title>
+                <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
+                <p style="color:#FFFF";>(error 666). El correo que ingresaste ya ha sido registrado....</p>
+                    <script>
+                    function r() { location.href="../../Vistas/skaters/registro.html"} 
+                    setTimeout ("r()", 2700);
+                    </script>
+                </head>
+                <body style="background-color:#494949;">
+                </body>
+            </html>
             ';
             $borrarIncorrectos = mysqli_query($conexion, "delete from usuario where Usuario_ID = '$nUsuario'") or die ("fallo al borrar rep");
         }
         $verifica=mysqli_fetch_array($verificaCorreo);
-    } 
+    }
     echo '
     <html>
         <head>
@@ -72,7 +72,7 @@ else{
         <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
         <p style="color:#FFFF";>El regsitro se ha completado exitosamente! ..Redireccionando a Login..</p>
             <script>
-            function r() { location.href="../Vistas/skaters/login.html"} 
+            function r() { location.href="../../Vistas/skaters/login.html"} 
             setTimeout ("r()", 2700);
             </script>
         </head>
@@ -80,6 +80,7 @@ else{
         </body>
     </html>
     ';
+    
 }
 mysqli_close($conexion);
 ?>
