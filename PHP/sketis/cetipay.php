f13e0af0b2f4dfbde60e35a6ed0eb944fac37859
KrozzSoulFire <56007887+KrozzSoulFire@users.noreply.github.com>
2019-10-03 19:40:39 -0500

    conexionsini al banquini

---
+++ PHP/sketis/cetipay.php
@@ -0,0 +1,16 @@
+<?PHP
+
+session_start();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, UPDATE,OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, X-HTTP-Method-Override,Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+$conexion=mysqli_connect("localhost","root","")
+or die("No se puede conectar con el servidor");
+
+mysqli_select_db($conexion, "cetipay")
+or die("No se puede conectar a la base de datos.");
+
+
+?>
