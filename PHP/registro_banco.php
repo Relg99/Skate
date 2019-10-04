e75354f8b0e3af4d56f49127ac09bfc50844d452
HumberHerbert <humbertojj.2c1@gmail.com>
2019-10-04 12:06:58 -0500

    ok

--- PHP/registro_banco.php
+++ PHP/registro_banco.php
@@ -1,35 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "banco(1)";
+include ('sketis/conexionBanco.php');
 $nombre;
 $apellido;
 $correo;
 $contraseña;
 
-$conexion = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-if (isset($_POST['nombre']) && $_POST['apellido'] && $_POST['correo'] && $_POST['contraseña']){
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $correo = $_POST['correo'];
-    aes_encrypt("contraseña",".SESSION['secret'].");
     $contraseña = $_POST['contraseña'];
-
-    $sql = "INSERT INTO usuarios (nombre, apellido, correo, contraseña)
-    VALUES ($nombre, $apellido, $correo, $contraseña)";
-}
-
-
-if ($conexion->query($sql) === TRUE) {
-    
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    $confirmar = $_POST['confirmar'];
+    $nUsuario=2;
+    $consulta = mysqli_query($conexion, "insert into usuarios (Nombre, Apellido, Correo, Contraseña, PK_usuarios)
+        values ( '$nombre', '$apellido', '$correo', '$contraseña', '$nUsuario')");
 
 $conexion->close();
 ?>
