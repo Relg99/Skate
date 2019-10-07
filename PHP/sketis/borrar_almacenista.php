ca75622a4e3bdc136704e078664e93b150202384
Rleg99 <Rleg99@users.noreply.github.com>
2019-10-06 23:47:13 -0500

    *Eliminar almacenista funcionando

--- PHP/sketis/borrar_almacenista.php
+++ PHP/sketis/borrar_almacenista.php
@@ -2,14 +2,24 @@
 
 include 'conexion.php'; 
 
+$Usuario_ID =$_POST['number'];
+$ID_Borrar;
 
-$Usuario_ID =$_POST["id"];
+$consulta= mysqli_query($conexion,'SELECT usuario_ID FROM usuario WHERE Tipo_FK = 3')
+or die("Fallo");
 
-$usuarioBD = mysqli_query($conexion, 'DELETE FROM `usuario` WHERE `usuario`.`Usuario_ID` = "'.$Usuario_ID.'"')
-or die("Fallo1");
+$nfilas=mysqli_num_rows($consulta);
+$Fila=mysqli_fetch_array($consulta);
 
+if($nfilas > 0){
+    for ($i=0;$i<$Usuario_ID+1;$i++){
+        $ID_Borrar = $Fila['usuario_ID'];
+        $Fila=mysqli_fetch_array($consulta);
+}
+}
 
+mysqli_query($conexion,'DELETE FROM usuario WHERE Usuario_ID = '.$ID_Borrar.';')
+or die("Fallo");
 
-
-	
+mysqli_close ($conexion);
 ?>
