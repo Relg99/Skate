7cde258e627ed17cf05c072bbcb21603d0fe6ec8
Liloth00814 <Liloth00814@gmail.com>
2019-10-16 16:29:12 -0500

    Resuelto error 15.No se esta registrando el almacenista (skaters)

--- PHP/sketis/regAlmacenistas.php
+++ PHP/sketis/regAlmacenistas.php
@@ -55,65 +55,64 @@ if ($contraseña != $confirmar){
 }
 
 else{
-    $cuentaFilas = mysqli_query($conexion, "select Usuario_ID from usuario") or die ("Fallo la consulta uno");
-    $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
 
     $verificaCorreo = mysqli_query($conexion, "select Correo from usuario") or die ("Fallo en la consulta correo");
     $verifica=mysqli_fetch_array($verificaCorreo);
-
-    $consulta = mysqli_query($conexion, "insert into usuario (Usuario_ID, Tipo_FK, Nombre, Apellido, Correo, Contrasena)
-    values ('$nUsuario', '3', '$nombre', '$apellido', '$correo', '$contraseña')")
-    or die("Fallo la consulta </br>");
-
-    $nUsuario=mysqli_num_rows($cuentaFilas) + 1;
-    for ($i = 1; $i <= $nUsuario; $i ++){
-        if ($correo == $verifica["Correo"]){
-            $estado = true;
-            echo '
-            <html>
-            <head>
-                    <meta charset="utf-8" />
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <title>Skaters - Status</title>
-                    <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
-                    <script>
-                            function r() { location.href="../../Vistas/skaters/registro-almacenista.html"}
-                            setTimeout ("r()", 5000);
-                            </script>
-                    <style>
-                    body{
-                        background-color: #ef7979;
-                        }
-                    .okimage{
-                        display:block;
-                        margin-left: auto;
-                        margin-right: auto;
-                        margin-top: 200px;
-                        height: 200px;
-                        width: 200px;
-                    }
-                    .texto{
-                        text-align: center;
-                        font: oblique bold 120% cursive;
-                        font-size: 200%;
-                        color: #FFF;
-                    }
-                    </style>
-            </head>
-            <body>
-                <tr>
-                    <td> <img class="okimage" src="../../Vistas/Assets/icons/err_icon.png"/> </td>
-                    <td><p class="texto">Error: El correo que ingresaste ya existe en el sistema
-                    </br> seras redireccionado automaticamente....</p></td>
-                </tr>
-            </body>
-        </html>
-            ';
-            $borrarIncorrectos = mysqli_query($conexion, "delete from usuario where Usuario_ID = '$nUsuario'") or die ("fallo al borrar rep");
-        }
-        $verifica=mysqli_fetch_array($verificaCorreo);
+    $cantidad=mysqli_num_rows($verificaCorreo);
+    for ($i=0; $i<=$cantidad; $i++)
+    {
+      if ($correo==$verifica["Correo"])
+      {
+        $estado=true;
+      }
+      $verifica=mysqli_fetch_array($verificaCorreo);
     }
-    if ($estado == false){
+    if ($estado==true){
+      echo '
+      <html>
+<head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Skaters - Status</title>
+      <link rel="shortcut icon" href="../../Vistas/Assets/icons/logo_header.png" />
+      <script>
+              function r() { location.href="../../Vistas/skaters/registro-almacenista.html"}
+              setTimeout ("r()", 3500);
+              </script>
+      <style>
+      body{
+          background-color: #ef7979;
+          }
+      .okimage{
+          display:block;
+          margin-left: auto;
+          margin-right: auto;
+          margin-top: 200px;
+          height: 200px;
+          width: 200px;
+      }
+      .texto{
+          text-align: center;
+          font: oblique bold 120% cursive;
+          font-size: 200%;
+          color: #FFF;
+      }
+      </style>
+</head>
+<body>
+  <tr>
+      <td> <img class="okimage" src="../../Vistas/Assets/icons/err_icon.png"/> </td>
+      <td><p class="texto">Error: El correo que ingresaste ya existe en el sistema
+       </br> seras redireccionado automaticamente....</p></td>
+  </tr>
+</body>
+</html>
+      ';
+  }
+  else { //Si no se encontro correo igual, permite el registro.
+      $consulta = mysqli_query($conexion, "insert into usuario (Tipo_FK, Nombre, Apellido, Correo, Contrasena)
+      values ('3', '$nombre', '$apellido', '$correo', '$contraseña')")
+      or die("Fallo la consulta </br>");
         echo '
         <html>
         <head>
@@ -148,7 +147,7 @@ else{
         <body>
             <tr>
                 <td> <img class="okimage" src="../../Vistas/Assets/icons/ok_icon.png"/> </td>
-                <td><p class="texto">Te has registrado de manera exitosa! Gracias por formar parte de skaters.
+                <td><p class="texto">Has registrado a un almacenista de manera exitosa!
                 </br> seras redireccionado automaticamente....</p></td>
             </tr>
         </body>
