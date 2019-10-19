c0c300f5426c60d0e8005756b1f92c4d04ea3371
deadchri5 <deadchri5h@gmail.com>
2019-10-19 12:40:55 -0500

    Borre la basura de la base de datos de Cetipay .....

--- PHP/cetipay/cetipay.sql
+++ PHP/cetipay/cetipay.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 11, 2019 at 07:43 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-10-2019 a las 19:39:55
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.1.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cetipay`
+-- Base de datos: `cetipay`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cuenta`
+-- Estructura de tabla para la tabla `cuenta`
 --
 
 CREATE TABLE `cuenta` (
@@ -35,7 +35,7 @@ CREATE TABLE `cuenta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `cuenta`
+-- Volcado de datos para la tabla `cuenta`
 --
 
 INSERT INTO `cuenta` (`PK_pago`, `Saldo`, `FK_user`) VALUES
@@ -48,50 +48,19 @@ INSERT INTO `cuenta` (`PK_pago`, `Saldo`, `FK_user`) VALUES
 (13, 1100, 18),
 (14, 1000, 19),
 (15, 9580, 20),
-(16, 1100, 21),
+(16, 987300, 21),
 (17, 1100, 22),
 (18, 720, 23),
 (23, 410, 28),
 (24, 200, 29),
 (25, 9200, 30),
 (27, 0, 32),
-(28, 0, 33),
-(29, 0, 34),
-(30, 0, 35),
-(31, 0, 36),
-(32, 0, 37),
-(33, 0, 38),
-(34, 0, 39),
-(35, 0, 40),
-(36, 0, 41),
-(37, 0, 42),
-(38, 0, 43),
-(39, 0, 44),
-(40, 0, 45),
-(41, 0, 46),
-(42, 0, 47),
-(43, 0, 48),
-(44, 0, 49),
-(45, 0, 50),
-(46, 0, 51),
-(47, 0, 52),
-(48, 0, 53),
-(49, 0, 54),
-(50, 0, 55),
-(51, 0, 56),
-(52, 0, 57),
-(53, 0, 58),
-(54, 0, 59),
-(55, 0, 60),
-(56, 0, 61),
-(57, 0, 62),
-(58, 0, 63),
-(59, 0, 64);
+(28, 0, 33);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -104,7 +73,7 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
 INSERT INTO `usuario` (`PK_usuarios`, `Nombre`, `Apellido`, `Telefono`, `Correo`, `Contrasena`) VALUES
@@ -124,78 +93,47 @@ INSERT INTO `usuario` (`PK_usuarios`, `Nombre`, `Apellido`, `Telefono`, `Correo`
 (29, 'Ma', 'Ji', 3311416392, 'a16100159@ceti.mx', '123'),
 (30, 'Herbert', 'Jaime', 3310489361, 'herbert@hotmail.com', '1234'),
 (32, 'Juan Pablo ', 'Rodriguez Ramirez', 3339507279, 'krozzimpulse@gmail.com', '123'),
-(33, 'juan', ' pa', 3311564293, 'a16100279', '123'),
-(34, '%%%%%', '%%%%%', 1, '%%%%%%%', '1234'),
-(35, '%%%%%', '%%%%%', 1, '%%%@%%%%', '1234'),
-(36, '%%%%%', '%%%%%', 1, '%%%@%%%%', '1234'),
-(37, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(38, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(39, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(40, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(41, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(42, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(43, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(44, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(45, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(46, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(47, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(48, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(49, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(50, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(51, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(52, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(53, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(54, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(55, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(56, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(57, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(58, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(59, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(60, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(61, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(62, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(63, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234'),
-(64, '%%%%%', '%%%%%', 1, 'jaimependejo@gmail.com', '1234');
+(33, 'juan', ' pa', 3311564293, 'a16100279', '123');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `cuenta`
+-- Indices de la tabla `cuenta`
 --
 ALTER TABLE `cuenta`
   ADD PRIMARY KEY (`PK_pago`),
   ADD KEY `FK_user` (`FK_user`);
 
 --
--- Indexes for table `usuario`
+-- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`PK_usuarios`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `cuenta`
+-- AUTO_INCREMENT de la tabla `cuenta`
 --
 ALTER TABLE `cuenta`
   MODIFY `PK_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
--- AUTO_INCREMENT for table `usuario`
+-- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `PK_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `cuenta`
+-- Filtros para la tabla `cuenta`
 --
 ALTER TABLE `cuenta`
   ADD CONSTRAINT `FK_user` FOREIGN KEY (`FK_user`) REFERENCES `usuario` (`PK_usuarios`) ON DELETE CASCADE ON UPDATE CASCADE;
