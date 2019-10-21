33bedda60631b107903f11dbcd1d9971fb645aad
HumberHerbert <humbertojj.2c1@gmail.com>
2019-10-21 18:25:12 -0500

    canjear codigo funcable, y saldo insuficiente retornando al carrito

--- PHP/cetipay/canjear_codigo.php
+++ PHP/cetipay/canjear_codigo.php
@@ -5,15 +5,22 @@ $codigo = $_POST["codigo"];
 $usuario= $_SESSION['IDPay'];
 if (isset ($codigo)){
 $rest = substr ($codigo, 0, 3);
-$consulta= mysqli_query($conexion, 'select saldo from cuenta where FK_user='.$usuario)
-or die ("Fallo la consulta");
-$total=$codigo;
-while ($row = mysqli_fetch_array ($consulta)){
-    $total = $total + $row['saldo'];
 }
-    $sql = mysqli_query ($conexion, 'update cuenta set Saldo ="' .$total.'" where FK_user='.$usuario);
-   print '{"success":true}';
-
+if (is_numeric($rest))
+{
+    $consulta= mysqli_query($conexion, 'select saldo from cuenta where FK_user='.$usuario)
+    or die ("Fallo la consulta");
+    $total=$rest;
+    while ($row = mysqli_fetch_array ($consulta)){
+        $total = $total + $row['saldo'];
+    }
+        $sql = mysqli_query ($conexion, 'update cuenta set Saldo ="' .$total.'" where FK_user='.$usuario);
+       print '{"success":true}';
+    
+    }else
+{
+    print '{"success":false}';
+    
 }
 $conexion->close();
 ?>
