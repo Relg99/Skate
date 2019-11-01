b06ef4f7fcc2760e01be989b5f467b2f8a1b5801
deadchri5 <deadchri5h@gmail.com>
2019-11-01 02:04:16 -0600

    Modifique metodo para editar productos, se va a tener que editar el alter table del html que habia hecho mario.

--- PHP/sketis/articulos_almacen.php
+++ PHP/sketis/articulos_almacen.php
@@ -5,10 +5,11 @@ include 'conexion.php';
 
 //Lista articulos almacen
 
-$consulta = mysqli_query($conexion,'select producto.Producto_ID as id, articulo.Nombre as tipo, producto.Nombre as nombre, 
-producto.Modelo as modelo, producto.Descripcion as descripcion, producto.Foto as foto, producto.Cantidad AS cantidad
- FROM articulo INNER JOIN producto 
-ON producto.Articulo_FK = articulo.Articulo_ID;')
+$consulta = mysqli_query($conexion,'select producto.Nombre as nombre, producto.Descripcion as descripcion, producto.Modelo as modelo, producto.Cantidad as cantidad,
+    producto.Precio as precio, producto.Foto as foto, marca.Nombre as marca, articulo.Nombre as tipo
+    from producto
+    inner join marca on producto.Marca_FK = marca.Marca_ID
+    inner join articulo on producto.Articulo_FK = articulo.Articulo_ID ;')
 or die("Fallo la consulta");
 
 $nfilas=mysqli_num_rows($consulta);
@@ -17,13 +18,14 @@ $Fila=mysqli_fetch_array($consulta);
     echo '[';
     for ($i=0;$i<$nfilas;$i++){
         echo '{';
-        echo '"id":"'.$Fila["id"].'",';
-        echo '"tipo":"'.$Fila["tipo"].'",';
         echo '"nombre":"'.$Fila["nombre"].'",';
-        echo '"modelo":"'.$Fila["modelo"].'",';
         echo '"descripcion":"'.$Fila["descripcion"].'",';
+        echo '"modelo":"'.$Fila["modelo"].'",';
+        echo '"cantidad":"'.$Fila["cantidad"].'",';
+        echo '"precio":"'.$Fila["precio"].'",';
         echo '"foto":"'.$Fila["foto"].'",';
-        echo '"cantidad":"'.$Fila["cantidad"].'"';
+        echo '"marca":"'.$Fila["marca"].'",';
+        echo '"tipo":"'.$Fila["tipo"].'"';
         if($i==$nfilas-1){
         echo "}";
         }else{
